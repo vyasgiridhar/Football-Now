557631a9ad30a5e30d045de959ddf6f6dd8e3909
vyasgiridhar <vyasgiridhar27@gmail.com>
2016-11-17 20:33:16 +0530

    Added layouts

---
+++ app/src/main/java/project/vyas/footballmanager/MatchActivity.java
@@ -0,0 +1,21 @@
+package project.vyas.footballmanager;
+
+import android.support.v7.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+/**
+ * A login screen that offers login via email/password.
+ */
+public class MatchActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_match_list);
+    }
+
+}
+
