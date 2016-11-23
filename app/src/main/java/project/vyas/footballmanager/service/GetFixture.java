1f0046db9aae802236c3b23d2d19fb9d3d8f0556
vyasgiridhar <vyasgiridhar27@gmail.com>
2016-11-24 00:57:35 +0530

    Adding AsyncTask to fill Views.

--- app/src/main/java/project/vyas/footballmanager/service/GetFixture.java
+++ app/src/main/java/project/vyas/footballmanager/service/GetFixture.java
@@ -7,11 +7,19 @@ import android.util.Log;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import project.vyas.footballmanager.adpater.FixtureListAdapter;
+import project.vyas.footballmanager.model.Fixture;
 
 /**
  * Created by vyas on 11/24/16.
@@ -28,6 +36,7 @@ public class GetFixture extends AsyncTask<Void, Void, Boolean> {
 
     int day;
     ListView lv;
+    ArrayList<Fixture> fixtures;
     private Context context;
     private ProgressDialog pd;
 
@@ -73,9 +82,25 @@ public class GetFixture extends AsyncTask<Void, Void, Boolean> {
                     .build();
             Response response = client.newCall(request).execute();
             String result = response.body().string();
-
+            JSONArray json = new JSONArray(result);
+            fixtures = new ArrayList<>();
+            for (int i = 0; i < json.length(); i++) {
+                Fixture f = new Fixture();
+                JSONObject object = json.getJSONObject(i);
+                f.setAwayScore(object.getInt("Home_team_score"));
+                f.setHomeScore(object.getInt("Away_team_score"));
+                f.setAwayTeam(object.getString("Away_team"));
+                f.setHomeTeam(object.getString("Home_team"));
+                DateFormat formatter = new SimpleDateFormat("dd-mm-yyyy");
+                f.setMatchDate(formatter.parse(object.getString("M_date")));
+                f.setGameWeek(object.getInt("Gameweek"));
+                f.setGameNo(object.getInt("Gameno"));
+                f.setLeagueCode(object.getString("League_code"));
+                fixtures.add(f);
+            }
         } catch (Exception e) {
             Log.d("Error : ", e.toString());
+            return false;
         }
 
         return true;
@@ -84,7 +109,8 @@ public class GetFixture extends AsyncTask<Void, Void, Boolean> {
     protected void onPostExecute(Boolean status) {
         pd.dismiss();
         if (status) {
-
+            FixtureListAdapter adapter = new FixtureListAdapter(fixtures, context);
+            lv.setAdapter(adapter);
         } else {
             Toast.makeText(context, "Network Error", Toast.LENGTH_LONG).show();
         }
